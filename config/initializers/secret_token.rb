a6fc978ec2c8a18056ac5bf521dc08d4cc26b313
Timm Z <timm-z@web.de>
2012-03-21 22:03:47 +0100

    rails project created and gitignore updated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asdf::Application.config.secret_token = 'dc407dca45df1ff846fa88b9a5d84ef05b32df4de0a9229fdf7e423a0c876ba88c424167a9ea982f8c81f805b320553e08c00927ec9e05393b196a9b4f2cb881'
